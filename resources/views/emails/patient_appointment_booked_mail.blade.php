174ca85589dce07a19ddf9ed7bf2d3694af321d3
Jean Pierre Velez <vcjpierre@gmail.com>
2023-10-31 00:15:45 -0400

    fix email

--- resources/views/emails/patient_appointment_booked_mail.blade.php
+++ resources/views/emails/patient_appointment_booked_mail.blade.php
@@ -10,7 +10,7 @@
     <div>
         <h2>Hola, <b>{{ $name }}</b></h2>
         <p>Su cita reservada con éxito en <b>{{ $date }}</b> entre <b>{{ $time }}</b>.</p>
-        <p>Si desea cancelar su cita haga click en el boton de abajo.</p> --}}
+        <p>Si desea cancelar su cita haga click en el boton de abajo.</p>
         <div style="display: flex;justify-content: center">
         <a href="{{ route('cancelAppointment',['patient_id'=>$patientId,'appointment_unique_id'=>$appointmentUniqueId]) }}" style="padding: 7px 15px;text-decoration: none;font-size: 14px;background-color: #df4645;font-weight: 500;border: none;border-radius: 8px;color: white">
                 Cancelar cita
